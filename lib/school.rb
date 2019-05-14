6866fe2889eb8fba9a7e436a278f41f0ba8de147
Cody Frank <cody.frank30@gmail.com>
2019-05-14 01:33:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,10 +5,9 @@ class School
   attr_reader :name
   def initialize(name)
     @name = name
+    @roster = {}
   end
-  def roster
-    roster = {}
-  end
+
   def add_student(name, grade)
     roster[grade] ||= []
     roster[grade] << name
